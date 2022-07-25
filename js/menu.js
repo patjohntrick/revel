eb8215b7beef4b9866689cef544887638a4900f6
John Patrick Resurreccion <patrickjohn.resurreccion18@gmail.com>
2022-07-25 12:36:20 +0800

    add jquery to menu page

--- js/menu.js
+++ js/menu.js
@@ -1,42 +1,52 @@
 $(document).ready(function () {
   // pizza
   $(".pizza").on("click", function () {
-    $(".pizza, .pizza-subheading").addClass("active");
+    $(".pizza, .pizza-subheading, .pizza-menu-data").addClass("active");
     $(
-      ".pancit, .sizzlers, .home-made, .meryenda, .shawarma, .pancit-subheading"
+      ".pancit, .sizzlers, .home-made, .meryenda, .shawarma, .pancit-subheading, .sizzlers-subheading, .home-made-subheading, .meryenda-subheading, .shawarma-subheading, .pancit-menu-data, .sizzlers-menu-data, .home-made-menu-data, .meryenda-menu-data, .shawarma-menu-data"
     ).removeClass("active");
   });
   // pancit
   $(".pancit").on("click", function () {
-    $(".pancit, .pancit-subheading").addClass("active");
+    $(".pancit, .pancit-subheading, .pancit-menu-data").addClass("active");
     $(
-      ".pizza, .sizzlers, .home-made, .meryenda, .shawarma, .sizzlers-subheading, .pizza-subheading"
+      ".pizza, .sizzlers, .home-made, .meryenda, .shawarma, .sizzlers-subheading, .pizza-subheading, .sizzlers-subheading, .home-made-subheading, .meryenda-subheading, .shawarma-subheading, .pizza-menu-data, .sizzlers-menu-data, .home-made-menu-data, .meryenda-menu-data, .shawarma-menu-data"
     ).removeClass("active");
   });
   // sizzlers
   $(".sizzlers").on("click", function () {
-    $(".sizzlers, .sizzlers-subheading").addClass("active");
+    $(".sizzlers, .sizzlers-subheading, .sizzlers-menu-data").addClass(
+      "active"
+    );
     $(
-      ".pancit, .pizza, .home-made, .meryenda, .shawarma, .pancit-subheading, .pizza-subheading"
+      ".pancit, .pizza, .home-made, .meryenda, .shawarma, .pancit-subheading, .pizza-subheading, .home-made-subheading, .meryenda-subheading, .shawarma-subheading, .pizza-menu-data, .pancit-menu-data, .home-made-menu-data, .meryenda-menu-data, .shawarma-menu-data"
     ).removeClass("active");
   });
   // home-made
   $(".home-made").on("click", function () {
-    $(".home-made").addClass("active");
-    $(".pancit, .sizzlers, .pizza, .meryenda, .shawarma").removeClass("active");
+    $(".home-made, .home-made-subheading, .home-made-menu-data").addClass(
+      "active"
+    );
+    $(
+      ".pancit, .sizzlers, .pizza, .meryenda, .shawarma, .pizza-menu-data, .pancit-menu-data, .pizza-subheading, .pancit-subheading, .sizzlers-subheading, .meryenda-subheading, .shawarma-subheading, .sizzlers-menu-data, .meryenda-menu-data, .shawarma-menu-data"
+    ).removeClass("active");
   });
   // meryenda
   $(".meryenda").on("click", function () {
-    $(".meryenda").addClass("active");
-    $(".pancit, .sizzlers, .home-made, .pizza, .shawarma").removeClass(
+    $(".meryenda, .meryenda-subheading, .meryenda-menu-data").addClass(
       "active"
     );
+    $(
+      ".pancit, .sizzlers, .home-made, .pizza, .shawarma, .pizza-subheading, .pancit-subheading, .sizzlers-subheading, .home-made-subheading, .shawarma-subheading, .pizza-menu-data, .pancit-menu-data, .home-made-menu-data, .sizzlers-menu-data, .shawarma-menu-data"
+    ).removeClass("active");
   });
   // shawarma
   $(".shawarma").on("click", function () {
-    $(".shawarma").addClass("active");
-    $(".pancit, .sizzlers, .home-made, .meryenda, .pizza").removeClass(
+    $(".shawarma, .shawarma-subheading, .shawarma-menu-data").addClass(
       "active"
     );
+    $(
+      ".pancit, .sizzlers, .home-made, .meryenda, .pizza, .pizza-subheading, .pancit-subheading, .sizzlers-subheading, .home-made-subheading, .meryenda-subheading, .pizza-menu-data, .pancit-menu-data, .sizzlers-menu-data, .home-made-menu-data, .meryenda-menu-data"
+    ).removeClass("active");
   });
 });
